ed9388978605a5d872dc180ba03602e5c422bc33
matca <mat.campous@gmail.com>
2025-07-29 11:40:37 +0200

    Rajout des divisibles par 3

--- exercises/4-fizzbuzz/test_fizzbuzz.py
+++ exercises/4-fizzbuzz/test_fizzbuzz.py
@@ -1,4 +1,16 @@
 from src import fizzbuzz
 
-# def test_fizzbuzz():
-#     assert fizzbuzz(1)
+def test_1_return_1():
+    assert fizzbuzz(1) == "1"
+
+def test_3_return_Fizz():
+    assert fizzbuzz(3) == "Fizz"
+
+def test_4_return_4():
+    assert fizzbuzz(4) == "4"
+
+def test_5_return_Buzz():
+    assert fizzbuzz(5) == "Buzz"
+
+def test_6_return_Fizz():
+    assert fizzbuzz(6) == "Fizz"
